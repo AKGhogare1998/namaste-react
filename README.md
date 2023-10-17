f55d6fb20f92ca5bac880fa049ad18e616a214b2
Akshay <akshayghogare98@gmail.com>
2023-10-17 23:01:45 +0530

    Episode 2 Igniting app using parcel

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # namaste react 
+
+# remove main.js from package.json as we are mentioning entry to app via command
+# Parcel
+  dev build 
+  local server
+  auto refreshing (HMR = )
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
 const parent = React.createElement("div",{id: "parent"},
 React.createElement("div",{id:"child"},[React.createElement("h1",{id:"h1"},"i am h1 tag"),React.createElement("h1",{id:"h1"},"i am h2 tag")]));
 console.log(parent)
