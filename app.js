9e1023281a2e017286aad153cffbf8ace0b3d646
Akshay <akshayghogare98@gmail.com>
2023-10-17 16:19:40 +0530

    Episode 1 Inception

---
+++ app.js
@@ -0,0 +1,7 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const parent = React.createElement("div",{id: "parent"},
+React.createElement("div",{id:"child"},[React.createElement("h1",{id:"h1"},"i am h1 tag"),React.createElement("h1",{id:"h1"},"i am h2 tag")]));
+console.log(parent)
+root.render(parent);
